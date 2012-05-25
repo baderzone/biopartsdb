1fcb27ac5309d3ce91e592304ed63a5aa23c1749
Emily Scher <scher.emily@gmail.com>
2012-05-25 13:51:36 -0400

    ligations working

--- app/controllers/ligations_controller.rb
+++ app/controllers/ligations_controller.rb
@@ -8,17 +8,25 @@ class LigationsController < ApplicationController
   end
 
   def new
+    @ligation_product = LigationProduct.new
     @ligation = Ligation.new
   end
   
   def create
+    @plasmid = Plasmid.find(params[:plasmid][:id])
+    
     @ligation = Ligation.new(params[:ligation])
+   
     @ligation.user = current_user
     @ligation.status = Status.find_by_process_and_default(Ligation.to_s, true)
     @ligation.protocol = Protocol.find_by_process(Ligation.to_s)
 
     if @ligation.save
-      redirect_to ligation_path(@ligation), :notice => "Ligations created correctly."
+      @ligation.ligation_products.each do |lig|
+        lig.plasmid = @plasmid
+      end
+      @ligation.save
+        redirect_to ligation_path(@ligation), :notice => "Ligations created correctly."
     else
       render :new, :error => "Ligation error."
     end
@@ -31,7 +39,7 @@ class LigationsController < ApplicationController
   end
   
   def update
-    @ligation = Ligation.find(params[:id])
+    @ligation = Ligation.find_by_name(params[@plasmid.name])
     if @ligation.update_attributes(params[:ligation])
       redirect_to ligation_path(@ligation), :notice => "Ligation updated correctly."
     else
--- app/models/plasmid.rb
+++ app/models/plasmid.rb
@@ -2,4 +2,8 @@ class Plasmid < ActiveRecord::Base
   belongs_to :vendor
   
   attr_accessible :name, :vendor
+  
+  def to_s
+    "#{name}"
+  end
 end
