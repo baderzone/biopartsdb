a1c388cfeb828d870a5731bfbb1ad2916d6eb5d3
Giovanni Stracquadanio <stracquadanio@jhu.edu>
2012-06-07 13:46:43 -0400

    Fixed minor UI bugs in the protocols admin

--- app/helpers/protocols_helper.rb
+++ app/helpers/protocols_helper.rb
@@ -1,7 +1,7 @@
 module ProtocolsHelper
   def markdown(content)
     Redcarpet::Markdown.new(Redcarpet::Render::HTML,
-            :autolink => true, :space_after_headers => true).render(content)
+            :autolink => true, :space_after_headers => true).render(content) unless content.nil?
   end
   
 end
