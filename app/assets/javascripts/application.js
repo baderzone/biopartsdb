3d62cecfdf73e4ced804b716b461a9a571d19597
Giovanni Stracquadanio <stracquadanio@jhu.edu>
2012-06-10 17:23:38 -0400

    minor changes to pcr gel images

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,6 +13,7 @@
 //= require jquery
 //= require jquery_ujs
 //= require jquery.ui.all
+//= require jquery.annotate
 //= require twitter/bootstrap
 //= require_tree .
 
--- app/controllers/pcr_gel_images_controller.rb
+++ app/controllers/pcr_gel_images_controller.rb
@@ -1,4 +1,9 @@
 class PcrGelImagesController < ApplicationController
+  
+  def show
+    @img = PcrGelImage.find(params[:id])
+  end
+  
   def create
     @img = PcrGelImage.new(params[:pcr_gel_image])
     @img.pcr_gel_id = params[:pcr_gel_id]
