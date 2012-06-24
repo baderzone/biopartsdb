4e9e0c73359a2a6494d0cbdb7d5fcf0886f09f12
Giovanni Stracquadanio <stracquadanio@jhu.edu>
2012-06-24 10:37:26 -0400

    fixed some security warnings just to be kind with brakeman

--- app/helpers/admin/protocols_helper.rb
+++ app/helpers/admin/protocols_helper.rb
@@ -1,6 +1,8 @@
 module Admin::ProtocolsHelper
   def markdown(content)
-    Redcarpet::Markdown.new(Redcarpet::Render::HTML,
-            :autolink => true, :space_after_headers => true).render(content)
+    #options = [:hard_wrap, :filter_html, :autolink, :no_intraemphasis, :fenced_code, :gh_blockcode]
+    #Redcarpet.new(content, *options).html_safe
+    #Redcarpet::Markdown.new(Redcarpet::Render::HTML,
+    #        :autolink => true, :space_after_headers => true).render(content)
   end
 end
--- app/helpers/protocols_helper.rb
+++ app/helpers/protocols_helper.rb
@@ -1,7 +1,7 @@
 module ProtocolsHelper
   def markdown(content)
     Redcarpet::Markdown.new(Redcarpet::Render::HTML,
-            :autolink => true, :space_after_headers => true).render(content) unless content.nil?
+           :autolink => true, :space_after_headers => true).render(content).html_safe unless content.nil?
   end
   
 end
